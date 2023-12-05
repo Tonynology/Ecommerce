500c57fdf387b68b99f2f8b14a0f9548d10610e9
moonthoon <tonycontinue@gmail.com>
2023-12-05 13:36:43 +0900

    feat: 로그인기능, 토큰발급기능 테스트코드 작성

--- src/test/java/project/Ecommerce/controller/UserControllerTest.java
+++ src/test/java/project/Ecommerce/controller/UserControllerTest.java
@@ -6,7 +6,9 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static project.Ecommerce.type.ErrorCode.INVALID_TOKEN;
 import static project.Ecommerce.type.ErrorCode.USER_ALREADY_EXIST;
+import static project.Ecommerce.type.ErrorCode.USER_NOT_FOUND;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
@@ -16,7 +18,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import project.Ecommerce.dto.ReIssue;
+import project.Ecommerce.dto.SignIn;
 import project.Ecommerce.dto.SignUp;
+import project.Ecommerce.exception.TokenException;
 import project.Ecommerce.exception.UserException;
 import project.Ecommerce.security.JwtTokenProvider;
 import project.Ecommerce.service.UserService;
@@ -86,5 +91,90 @@ public class UserControllerTest {
         .andExpect(jsonPath("$.errorCode").value("USER_ALREADY_EXIST"))
         .andExpect(jsonPath("$.errorMessage").value("이미 존재하는 회원입니다"))
         .andExpect(status().isOk());
+  }
+
+
+  @Test
+  @DisplayName("로그인 성공")
+  void successSignIn() throws Exception {
+    //given
+    String accessToken = "accessToken";
+    String refreshToken = "refreshToken";
+
+    SignIn.Request request = SignIn.Request.builder()
+        .email("jason@gmail.com")
+        .password("abc1234")
+        .build();
+
+    SignIn.Response response = SignIn.Response.builder()
+        .accessToken(accessToken)
+        .refreshToken(refreshToken)
+        .build();
+
+    given(userService.signIn(any()))
+        .willReturn(response);
+
+    //when
+    //then
+    mockMvc.perform(post("/user/signin")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(request)))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.accessToken").value(accessToken))
+        .andExpect(jsonPath("$.refreshToken").value(refreshToken))
+        .andDo(print());
+  }
+
+  @Test
+  @DisplayName("로그인 실패 - 존재하지 않는 이메알")
+  void failSignIn() throws Exception {
+    //given
+    SignIn.Request request = SignIn.Request.builder()
+        .email("jason@gmail.com")
+        .password("abc1234")
+        .build();
+
+    given(userService.signIn(any()))
+        .willThrow(new UserException(USER_NOT_FOUND));
+
+    //when
+    //then
+    mockMvc.perform(post("/user/signin")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(request)))
+        .andDo(print())
+        .andExpect(jsonPath("$.errorCode").value("USER_NOT_FOUND"))
+        .andExpect(jsonPath("$.errorMessage").value("회원가입이 안된 이메일입니다"))
+        .andExpect(status().isOk());
+  }
+
+  @Test
+  @DisplayName("토큰 재발급 성공")
+  void successReIssue() throws Exception {
+    //given
+    String accessToken = "accessToken";
+    String refreshToken = "refreshToken";
+
+    ReIssue.Request request = ReIssue.Request.builder()
+        .refreshToken(refreshToken)
+        .build();
+
+    ReIssue.Response response = ReIssue.Response.builder()
+        .accessToken(accessToken)
+        .refreshToken(refreshToken)
+        .build();
+
+    given(userService.reIssue(any()))
+        .willReturn(response);
+
+    //when
+    //then
+    mockMvc.perform(post("/user/reissue")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(request)))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.accessToken").value(accessToken))
+        .andExpect(jsonPath("$.refreshToken").value(refreshToken))
+        .andDo(print());
   }
 }
--- src/test/java/project/Ecommerce/service/UserServiceTest.java
+++ src/test/java/project/Ecommerce/service/UserServiceTest.java
@@ -6,6 +6,10 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -13,9 +17,16 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import project.Ecommerce.dto.ReIssue;
+import project.Ecommerce.dto.SignIn;
 import project.Ecommerce.dto.SignUp;
 import project.Ecommerce.entity.User;
+import project.Ecommerce.exception.TokenException;
+import project.Ecommerce.exception.UserException;
+import project.Ecommerce.redis.RefreshToken;
+import project.Ecommerce.redis.RefreshTokenRepository;
 import project.Ecommerce.repository.UserRepository;
+import project.Ecommerce.security.JwtTokenProvider;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
@@ -24,12 +35,19 @@ public class UserServiceTest {
   private UserRepository userRepository;
 
   @Mock
+  private RefreshTokenRepository refreshTokenRepository;
+
+  @Mock
   private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+  @Mock
+  private JwtTokenProvider jwtTokenProvider;
 
   @InjectMocks
   private UserServiceImpl userServiceImpl;
 
   @Test
+  @DisplayName("회원가입 성공")
   void successSignUp() {
     //given
     SignUp.Request request = SignUp.Request.builder()
@@ -56,6 +74,118 @@ public class UserServiceTest {
     verify(userRepository, times(1)).save(captor.capture());
     assertEquals("Jason", response.getName());
     assertEquals("Jason", captor.getValue().getName());
+  }
 
+  @Test
+  @DisplayName("로그인 성공")
+  void successSignIn() {
+    //given
+    String email = "Jason@gmail.com";
+    String password = "abc1234";
+    String accessToken = "accessToken";
+    String refreshToken="refreshToken";
+
+    SignIn.Request request = SignIn.Request.builder()
+        .email(email)
+        .password(password)
+        .build();
+
+    User mockUser = new User();
+    mockUser.setEmail(email);
+    mockUser.setPassword(password);
+
+    when(userRepository.findUserByEmail(email)).thenReturn(Optional.of(mockUser));
+    when(bCryptPasswordEncoder.matches(password, mockUser.getPassword())).thenReturn(true);
+    when(jwtTokenProvider.createAccessToken(email)).thenReturn(accessToken);
+    when(jwtTokenProvider.createRefreshToken(email)).thenReturn(refreshToken);
+
+    //when
+    SignIn.Response response = userServiceImpl.signIn(request);
+
+    //then
+    assertEquals(accessToken, response.getAccessToken());
+    assertEquals(refreshToken, response.getRefreshToken());
+  }
+
+  @Test
+  @DisplayName("로그인 실패 - 사용자를 찾을 수 없음")
+  void failSignInUserNotFound() {
+    // given
+    String email = "nonexisting@example.com";
+    String password = "abc1234";
+
+    SignIn.Request request = SignIn.Request.builder()
+        .email(email)
+        .password(password)
+        .build();
+
+    when(userRepository.findUserByEmail(email)).thenReturn(Optional.empty());
+
+    //when
+    //then
+    assertThrows(UserException.class, () -> userServiceImpl.signIn(request));
+  }
+
+  @Test
+  @DisplayName("로그인 실패 - 잘못된 비밀번호")
+  void failSignInIncorrectPassword() {
+    //given
+    String email = "Jason@gmail.com";
+    String password = "wrongPassword";
+
+    User mockUser = new User();
+    mockUser.setEmail(email);
+    mockUser.setPassword(password);
+
+    SignIn.Request request = SignIn.Request.builder()
+        .email(email)
+        .password(password)
+        .build();
+
+    when(userRepository.findUserByEmail(email)).thenReturn(Optional.of(mockUser));
+    when(bCryptPasswordEncoder.matches(password, mockUser.getPassword())).thenReturn(false);
+
+    // when
+    // then
+    assertThrows(UserException.class, () -> userServiceImpl.signIn(request));
+  }
+
+  @Test
+  @DisplayName("토큰 재발급 성공")
+  void successReIssue() {
+    // Given
+    String email = "user@example.com";
+    String refreshToken = "refreshToken";
+    String newAccessToken = "newAccessToken";
+    RefreshToken mockRefreshToken = new RefreshToken(refreshToken, email);
+
+    ReIssue.Request request = ReIssue.Request.builder()
+        .refreshToken(refreshToken)
+        .build();
+
+    when(refreshTokenRepository.findByEmail(refreshToken)).thenReturn(Optional.of(mockRefreshToken));
+    when(jwtTokenProvider.createAccessToken(email)).thenReturn(newAccessToken);
+
+    // When
+    ReIssue.Response response = userServiceImpl.reIssue(request);
+
+    // Then
+    assertEquals(newAccessToken, response.getAccessToken());
+    assertEquals(refreshToken, response.getRefreshToken());
+  }
+
+  @Test
+  @DisplayName("토큰 재발급 실패 - 유효하지 않은 토큰")
+  void failReIssueInvalidToken() {
+    // Given
+    String refreshToken = "invalidToken";
+    ReIssue.Request request = ReIssue.Request.builder()
+        .refreshToken(refreshToken)
+        .build();
+
+    when(refreshTokenRepository.findByEmail(refreshToken)).thenReturn(Optional.empty());
+
+    // When & Then
+    assertThrows(TokenException.class, () -> userServiceImpl.reIssue(request));
   }
 }
