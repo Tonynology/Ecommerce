1d0d1d20856fce228fe3f2dd1513c1ee893c10e7
moonthoon <tonycontinue@gmail.com>
2023-12-11 22:54:51 +0900

    chores: categoryType 영문, 한글 설정

--- src/main/java/project/Ecommerce/type/CategoryType.java
+++ src/main/java/project/Ecommerce/type/CategoryType.java
@@ -1,5 +1,14 @@
 package project.Ecommerce.type;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public enum CategoryType {
-  FASHION, FOOD, TOOLS, BEAUTY, PETS
+  FASHION("패션"),
+  FOOD("음식"),
+  TOOLS("문구"),
+  BEAUTY("뷰티"),
+  PETS("애견"),
+  ;
+  private final String categoryKor;
 }
